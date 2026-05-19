4962ae3acbad242c39e056d24220ed4445af22c2
Anas Ayman <anasaymanAY@gmail.com>
2026-05-19 15:38:11 +0300

    edit in import images

--- src/Components/projects/projectsData.jsx
+++ src/Components/projects/projectsData.jsx
@@ -1,10 +1,18 @@
+import recipeImg from "../assets/recipe-search-project.jpeg";
+import dashboardImg from "../assets/Dashboard.jpeg";
+import portfolioImg from "../assets/portfolio.jpeg";
+import todoImg from "../assets/to-do-list.jpeg";
+import fruitImg from "../assets/fruit-shop.jpeg";
+import upflowImg from "../assets/up-flow.jpeg";
+import crudsImg from "../assets/cruds.jpeg";
+
 export const projectsData = [
   {
     title: "Recipe Search App",
     description:
       "A meal search application with recipes, ingredients, and interactive food browsing.",
     icon: <i className="fa-solid fa-utensils"></i>,
-    image: "/src/assets/recipe-search-project.jpeg",
+    image: recipeImg,
     skills: ["HTML", "CSS", "JS"],
     liveDemo: "https://anasaymanay-dev.github.io/recipe-search-app/",
     repoLink: "https://github.com/anasaymanay-dev/recipe-search-app",
@@ -16,7 +24,7 @@ export const projectsData = [
     description:
       "A responsive dashboard featuring analytics, statistics cards, charts, and organized layouts.",
     icon: <i className="fa-solid fa-chart-line"></i>,
-    image: "/src/assets/Dashboard.jpeg",
+    image: dashboardImg,
     skills: ["HTML", "CSS"],
     liveDemo: "https://anasaymanay-dev.github.io/templete-4/",
     repoLink: "https://github.com/anasaymanay-dev/templete-4",
@@ -28,7 +36,7 @@ export const projectsData = [
     description:
       "A modern portfolio website with responsive layouts and smooth interactive experiences.",
     icon: <i className="fa-solid fa-user"></i>,
-    image: "/src/assets/portfolio.jpeg",
+    image: portfolioImg,
     skills: ["React", "CSS"],
     liveDemo: "https://react-portfolio-flax-alpha.vercel.app/",
     repoLink: "https://github.com/anasaymanay-dev/React-Portfolio",
@@ -40,7 +48,7 @@ export const projectsData = [
     description:
       "A task management application with filtering, tracking, and completion status features.",
     icon: <i className="fa-solid fa-list-check"></i>,
-    image: "/src/assets/to-do-list.jpeg",
+    image: todoImg,
     skills: ["React", "CSS"],
     liveDemo: "https://react-todo-app-zeta-red.vercel.app/",
     repoLink: "https://github.com/anasaymanay-dev/react-todo-app",
@@ -52,7 +60,7 @@ export const projectsData = [
     description:
       "A responsive fruit shop website with clean layouts and modern interfaces.",
     icon: <i className="fa-solid fa-apple-whole"></i>,
-    image: "/src/assets/fruit-shop.jpeg",
+    image: fruitImg,
     skills: ["HTML", "CSS"],
     liveDemo: "https://anasaymanay-dev.github.io/fruit-shop-website/",
     repoLink: "https://github.com/anasaymanay-dev/fruit-shop-website",
@@ -64,18 +72,19 @@ export const projectsData = [
     description:
       "A business landing website with modern sections and responsive user experiences.",
     icon: <i className="fa-solid fa-arrow-trend-up"></i>,
-    image: "/src/assets/up-flow.jpeg",
+    image: upflowImg,
     skills: ["HTML", "CSS"],
     liveDemo: "https://anasaymanay-dev.github.io/up-flow/",
     repoLink: "https://github.com/anasaymanay-dev/up-flow",
     search: "static",
   },
+
   {
     title: "CRUDS",
     description:
       "A product management application with search, totals, and CRUD functionality features.",
     icon: <i className="fa-solid fa-database"></i>,
-    image: "/src/assets/cruds.jpeg",
+    image: crudsImg,
     skills: ["HTML", "CSS", "JS"],
     liveDemo: "https://anasaymanay-dev.github.io/CRUDS/",
     repoLink: "https://github.com/anasaymanay-dev/CRUDS/tree/master",
